7d3986974ac79c9608d11019c7bb51896ed812f8
KaustubhSathe <f2015466p@alumni.bits-pilani.ac.in>
2022-01-01 03:36:26 +0530

    change

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 #### Services
 1. **rw-gql** : Edge layer
 2. **rw-user-service** : User Service includes AuthN/AuthZ
-3. **rw-article-service** : Article Service
+3. **rw-article-service** : Article Service 
 4. **rw-feed-service** : News feed service
 5. **rw-notifications** : Notification service
 6. **rw-search** : Search service
--- rw-gql/src/main/java/com/rw/gql/fetchers/ExampleFetcher.java
+++ rw-gql/src/main/java/com/rw/gql/fetchers/ExampleFetcher.java
@@ -3,16 +3,12 @@ package com.rw.gql.fetchers;
 import com.kaustubh.vertx.gql.starter.AbstractDataFetcher;
 import com.kaustubh.vertx.gql.starter.Fetcher;
 import com.kaustubh.vertx.gql.starter.Request;
-import com.kaustubh.vertx.gql.starter.io.Response;
-import graphql.schema.DataFetchingEnvironment;
 import io.reactivex.rxjava3.core.Single;
 
 
-import java.util.concurrent.CompletionStage;
-
 @Fetcher(
   consumes = "*",
-  parameter = "hello",
+  field = "hello",
   type = "Query"
 )
 public class ExampleFetcher extends AbstractDataFetcher<String> {
